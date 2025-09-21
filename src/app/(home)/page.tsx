108ca51eb87df91d2bc4c28605a28743d531a202
im-madoka <im.leojia@icloud.com>
2025-09-21 11:37:28 +0800

    Update homepage layout and add icon and welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,17 @@
-import Link from 'next/link';
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+    <main className="flex flex-1 flex-col justify-center items-center px-4">
+      <Image
+        src="/icon.png"
+        alt="sithra-rs icon"
+        width={100}
+        height={100}
+        className="rounded-full my-4 mt-8 md:mt-4"
+      />
+      <h1 className="mb-2 text-2xl font-bold">Hello World!</h1>
+      <h2 className="mb-2 font-bold">欢迎来到东大成贤计算机协会指南</h2>
     </main>
   );
 }
