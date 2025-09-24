061d9cd58a9cde2458c23bca38bb697b0713300c
im-madoka <im.leojia@icloud.com>
2025-09-25 01:32:05 +0800

    feat: 在首页增加导航

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,5 @@
+import { Card, Cards } from "fumadocs-ui/components/card";
+import { HammerIcon, HomeIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function HomePage() {
@@ -11,7 +13,18 @@ export default function HomePage() {
         className="rounded-full my-4 mt-8 md:mt-4"
       />
       <h1 className="mb-2 text-2xl font-bold">Hello World!</h1>
-      <h2 className="mb-2 font-bold">欢迎来到东大成贤计算机协会指南</h2>
+      <h2 className="mb-10 font-bold">欢迎来到东大成贤计算机协会指南</h2>
+      <Cards className="max-w-200 w-full">
+        <Card icon={<HomeIcon />} title={"成贤计协总览"} href={"/docs/global"}>
+          东大成贤计算机协会总览
+        </Card>
+        <Card icon={<HammerIcon />} title={"C/C++ 基础指南"} href={"/docs/cpp"}>
+          学习 C/C++ 的基础知识
+        </Card>
+        <Card icon={<HammerIcon />} title={"其他"} href={"/docs/others"}>
+          其他乱七八糟的教程
+        </Card>
+      </Cards>
     </main>
   );
 }
